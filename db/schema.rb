f512ceb101cc26f3395c73317570295eca4e6646
MasatoArima <afoc.0707@gmail.com>
2021-08-29 15:24:46 +0000

    [add]Bookモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_084523) do
+ActiveRecord::Schema.define(version: 2021_08_29_151917) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
