18b24b99ca178df9c09401ebc1ad0947fd159d4b
MasatoArima <afoc.0707@gmail.com>
2021-08-29 22:45:11 +0000

    [Add]bookコントローラ

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_29_151917) do
+ActiveRecord::Schema.define(version: 2021_08_29_163851) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
