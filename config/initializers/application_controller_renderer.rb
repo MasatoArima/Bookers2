41c2b119994c8a6837f40590ba8dbdc48780d676
MasatoArima <afoc.0707@gmail.com>
2021-08-30 03:52:24 +0000

    [Add]topページレイアウト

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2f02c0be0bd081ba2b3d69486ad595cdff6ea47f980d65ad337e521fd920c8f1e281fd9d0b296f2a7b4ab5a3b673ce24d3e9b31aa9542ff2e023bfdb1304e973'
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'homes/about'
   devise_for :users
   root to: 'homes#top'
   resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :users, only: [:show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
