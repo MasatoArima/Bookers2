80fee72759b6bcda39b3f80422704dac8421d90c
MasatoArima <afoc.0707@gmail.com>
2021-08-31 03:25:32 +0000

    [Add]レイアウト、リンク先追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @book = Book.new
   end
 
   def show
@@ -22,6 +23,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :body)
   end
 end
