f4c4bf8868139be3b7f76e5053799d01bfbde763
MasatoArima <afoc.0707@gmail.com>
2021-09-01 14:17:09 +0000

    [Fix]リンク先、インスタンス変数の修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,14 @@
 class BooksController < ApplicationController
-  # def new
-  #   @book = Book.new
-  # end
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+    @newbook = Book.new
+    @user = User.find(@book.user_id)
+  end
 
   def create
     @book = Book.new(book_params)
@@ -10,29 +17,20 @@ class BooksController < ApplicationController
     redirect_to book_path(@book)
   end
 
-  def index
-    @books = Book.all
-    @book = Book.new
-  end
-
   def edit
     @book = Book.find(params[:id])
-  end
-
-  def show
-    @book = Book.find(params[:id])
-  end
-
-  def destroy
-    @book = Book.find(params[:id])
-    @book.destroy
-    redirect_to books_path
   end
 
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
     redirect_to book_path(@book.id)
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
+    @user = User.find(current_user.id)
     @book = Book.new
   end
 
@@ -23,6 +24,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :profile_image, :body)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
